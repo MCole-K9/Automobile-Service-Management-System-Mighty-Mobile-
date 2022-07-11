7fef26b92a12c8cff115cffd0d3c44eb6862c352
Matthew Cole <m.b.cole300@gmail.com>
2022-07-11 15:08:40 -0500

    extended colours

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html","./src/**/*.{vue,ts,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ourYellow": "#F2B807",
+        "ourRed": "#D91828",
+        "ourGrey": "F2F2F2",
+        "ourBlue": "#2678BF"
+      }
+    },
   },
   daisyui: {
     themes: ["light"],
