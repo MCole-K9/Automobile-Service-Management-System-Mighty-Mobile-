b83cc3704c1a1eb31ca7c19514d4b88e09f3f9fc
Elliot Morrison <elliotmorrison58@gmail.com>
2022-07-19 12:16:42 -0500

    added jobdetails view

---
+++ frontend/src/views/JobDetailsView.vue
@@ -0,0 +1,22 @@
+<template>
+    <span>
+        <NavBar/>
+        <Job :job="g"/>
+    </span>
+</template>
+<script lang="ts">
+import NavBar from "@/components/NavBar.vue"
+import Job from "../components/Job.vue"
+    export default{
+        name:'JobDetailsView',
+        components:{
+            NavBar,
+            Job
+        },
+        data(){
+            return{
+                g : {}//just to get rid of props error
+            }
+        }
+    }
+</script>
