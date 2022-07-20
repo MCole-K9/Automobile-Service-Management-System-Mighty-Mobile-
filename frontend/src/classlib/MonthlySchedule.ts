fa8acb2acf396f328bf2856a81e876dab50d9672
khalid sheriff <khalalimsheriff@gmail.com>
2022-07-20 03:00:52 -0500

    even FURTHER attempts at finishing createMonthObject() (bruh)

--- frontend/src/classlib/MonthlySchedule.ts
+++ frontend/src/classlib/MonthlySchedule.ts
@@ -8,14 +8,20 @@ type MonthBlock = {
 
 
 class HourBlock {
-    time: Date;
+    time: number | null;
     description: string;
     duration: number;
+    id: number | null;
+    client: string;
+    blocktype: "APPOINTMENT" | "JOBSTAGE" | null;
 
     constructor(){
-        this.time = new Date()
+        this.time = null;
         this.description = "";
         this.duration = 1;
+        this.id = null;
+        this.client = "";
+        this.blocktype = null;
     }
     
 }
@@ -29,7 +35,7 @@ type DayBlock = {
 // Returns an object that holds all of the remaining days of the month
 // (from the current day), along with all of their scheduled events and/or
 // free blocks for scheduling
-export default async function createMonthObject(month: number, currentDate: Date): MonthBlock {
+export default function createMonthObject(month: number, currentDate: Date): MonthBlock {
     const monthlySchedule = {} as MonthBlock;
     
     try{
@@ -120,71 +126,153 @@ export default async function createMonthObject(month: number, currentDate: Date
         // needs double-checking
         const currentUser = newUserStore();
 
-
         // this should be able to generate a default list of however many blocks are
         // available in one working day (9h, 8AM->5PM)
 
         const prisma = new PrismaClient();
 
-        // Get all of the scheduled items for the month of
-        const schedule = await prisma.schedule.findMany({
-            
-            select: {
-                // so this should pull:
-                // -id:
-                // - date
-                // - appointment
-                //      - description
-                //      - customerId
-                //      - address
-                // - jobstage
-                //      - customerId
-                //      - description
-                //      - address
-                //
-            },        
-            
-            where: {
-                AND: [
-                    {
-                        date: {
-                            lte: chosenMonth.getFullYear() + '-' + chosenMonth.getMonth() + '-01',
-                            gte: chosenMonth.getFullYear() + '-' + chosenMonth.getMonth() + '-' + daysInMonth
-                        }
-                    },
-                    {
-                        OR: [
-                            {
-                                appointment: {
-                                    assignedMechId: currentUser.User.id
+        async function getSchedule(){
+            return await prisma.schedule.findMany({
+                orderBy: {
+                    date: 'asc'
+                },
+                //  but since a schedule only has appt OR jobstage, kinda wondering what happens when i try to pull for both
+                select: {
+                    id: true,
+                    date: true,
+                    appointment: {
+                        select: {
+                            problemDescription: true,
+                            customer: {
+                                select: {
+                                    id: true,
+                                    firstName: true,
+                                    lastName: true
                                 }
                             },
-
-                            {
-                                jobStage: {
-                                    job: {
-                                        assignedMechanicId: currentUser.User.id
+                            streetAddress: true,
+                            town: true,
+                            parish: true
+                        }
+                    },
+                    jobStage: {
+                        select: {
+                            description: true,
+                            duration: true,
+                            job: {
+                                select: {
+                                    jobNumber: true,
+                                    streetAddress: true,
+                                    town: true,
+                                    parish: true,
+                                    vehicle: {
+                                        select: {
+                                            owner: {
+                                                select: {
+                                                    id: true,
+                                                    firstName: true,
+                                                    lastName: true
+                                                }
+                                            }
+                                        }
                                     }
                                 }
                             }
-                        ] 
+                        }
                     }
-                ]
-            }
+                },        
+                
+                where: {
+                    AND: [
+                        {
+                            date: {
+                                lte: chosenMonth.getFullYear() + '-' + chosenMonth.getMonth() + '-01',
+                                gte: chosenMonth.getFullYear() + '-' + chosenMonth.getMonth() + '-' + daysInMonth
+                            }
+                        },
+                        {
+                            OR: [
+                                {
+                                    appointment: {
+                                        assignedMechId: currentUser.User.id
+                                    }
+                                },
+    
+                                {
+                                    jobStage: {
+                                        job: {
+                                            assignedMechanicId: currentUser.User.id
+                                        }
+                                    }
+                                }
+                            ] 
+                        }
+                    ]
+                }
+            });
+        }
+
+        // Get all of the scheduled items for the month
+        const schedule: any | null = getSchedule().then(value => value).catch(reason=> null); // worrying about errors later
+
+        let incrementor: number = 0;
+
+        // Put the scheduled items into their corresponding hour blocks of every working day
+        monthlySchedule.workingDays.forEach((workingDay)=>{
+            // need to check if schedule is null before i do this, but that's for later
+            do {
+                if(workingDay.day == schedule[incrementor].date.getDate()){
+                    // this means that it's a jobStage
+                    if (schedule[incrementor].jobStage !== null){
+                        let hourBlock = new HourBlock();
+
+                        hourBlock.blocktype = "JOBSTAGE";
+                        hourBlock.id = schedule[incrementor].id;
+
+                        hourBlock.client = schedule[incrementor].jobStage.job.vehicle.owner.firstName + " " +
+                            schedule[incrementor].jobStage.job.vehicle.owner.lastName;
+
+                        hourBlock.duration = schedule[incrementor].jobStage.duration;
+                        hourBlock.time = schedule[incrementor].date.getHours();
+                        hourBlock.description = schedule[incrementor].jobStage.description;
+
+                        // leaving out the address on purpose, since i might choose to leave it out on the UI
+
+                    }
+                    // this means that it's an appointment
+                    else{
+                        let hourBlock = new HourBlock();
+
+                        hourBlock.blocktype = "APPOINTMENT";
+                        hourBlock.id = schedule[incrementor].id;
+
+                        hourBlock.client = schedule[incrementor].appointment.customer.firstName + " " +
+                            schedule[incrementor].appointment.customer.lastName;
+
+                        hourBlock.duration = 1;
+                        hourBlock.time = schedule[incrementor].date.getHours();
+                        hourBlock.description = schedule[incrementor].appointment.problemDescription;
+
+                        // leaving out the address on purpose, since i might choose to leave it out on the UI
+                    }
+
+                    incrementor++;
+                }
+                else{
+                    incrementor++;
+                    break;
+                }
+            }while(true);
+
+            // code here needs to determine how many more spaces i need to fill with empty blocks
         });
 
-        // default values imply
-
-
-        // once every day added to the month object, check each day to see which
-        // hours are taken and available 
-        for (let day in monthlySchedule.workingDays){
-            // This should probably be one API call, rather than several ones
-        }
+        // unsure whether to handle the padding of hourblocks in a dayblock here or in the other function
+        // 
 
     }
     catch(functionError){
-        alert(functionError);
+        console.error(functionError);
     }
 
     return monthlySchedule;
