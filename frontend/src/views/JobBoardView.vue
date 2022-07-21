7e61d2ea46f1d50f2b850b887bc547309fa370bc
Elliot Morrison <elliotmorrison58@gmail.com>
2022-07-21 15:42:09 -0500

    add button design

--- frontend/src/views/JobBoardView.vue
+++ frontend/src/views/JobBoardView.vue
@@ -28,8 +28,9 @@
         <section id="Jobs"  class="mt-8">
             <component  :is="activeTab"/>
         </section>
-        <button class="btn  bg-ourYellow rounded-full w-12 h-12 fixed bottom-4 right-4">
-            <svg class="w-12 h-12" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+        <button class="btn bg-ourYellow flex items-center w-fit h-12 fixed bottom-4 right-4">
+            <svg class="w-10 h-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+            Add Job
         </button>
     </template>
 </DashboardLayout>
