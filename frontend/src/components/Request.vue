96b47cf8f332f0a2440e372de7effdc00065af5e
Matthew Cole <m.b.cole300@gmail.com>
2022-07-25 17:26:44 -0500

    Polymorphic Request Draft

---
+++ frontend/src/components/Request.vue
@@ -0,0 +1,103 @@
+<script setup lang="ts">
+
+    import type { Appointment, Job } from "@/classlib/Types"
+
+    const props = defineProps<{
+        job?: Job
+        appointment?: Appointment,
+
+    }>()
+
+</script>
+<template>
+ <!-- if is an Appointment -->
+    <tr v-if="props.appointment">
+        <th>
+            <label>
+                <input type="checkbox" class="checkbox" />
+            </label>
+        </th>
+        <td>
+            <div class="flex items-center space-x-3">
+                <div class="avatar">
+                    <!-- <div class="mask mask-squircle w-12 h-12">
+                                            <img src="/tailwind-css-component-profile-2@56w.png"
+                                                alt="Avatar Tailwind CSS Component" />
+                                           
+                                        </div> -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </div>
+                <div>
+                    <div class="font-bold">
+                        {{ props.appointment.customer?.firstName }}
+                        {{ props.appointment.customer?.lastName }}
+                    </div>
+                    <div class="text-sm opacity-50">Customer</div>
+                </div>
+            </div>
+        </td>
+        <td>
+            Assessment of {{ props.appointment.vehicle?.year }} {{ props.appointment.vehicle?.make }}
+            {{ props.appointment.vehicle?.model }}
+            <br>
+            <span class="badge badge-ghost badge-sm">Appointment</span>
+        </td>
+        <td>{{ props.appointment.suggestedDate }}</td>
+        <th>
+            <button class="btn btn-ghost btn-xs">Details</button>
+        </th>
+    </tr>
+
+    <!-- if is a Job -->
+     <tr v-if="props.job">
+        <th>
+            <label>
+                <input type="checkbox" class="checkbox" />
+            </label>
+        </th>
+        <td>
+            <div class="flex items-center space-x-3">
+                <div class="avatar">
+                    <!-- <div class="mask mask-squircle w-12 h-12">
+                                            <img src="/tailwind-css-component-profile-2@56w.png"
+                                                alt="Avatar Tailwind CSS Component" />
+                                           
+                                        </div> -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                </div>
+                <div>
+                    <div class="font-bold">
+                        {{ props.job.createdBy?.firstName }}
+                        {{ props.job.createdBy?.lastName }}
+                    </div>
+                    <div class="text-sm opacity-50">Mighty Mobile</div>
+                </div>
+            </div>
+        </td>
+        <td>
+            {{props.job.serviceType}} of {{ props.job.vehicle?.year }} {{ props.job.vehicle?.make }}
+            {{ props.job.vehicle?.model }}
+            <br>
+            <span class="badge badge-ghost badge-sm">Job Request</span>
+        </td>
+        <td>{{ props.job.startDate }}</td>
+        <th>
+            <button  class="btn btn-ghost btn-xs">Details</button>
+        </th>
+    </tr>
+</template>
+<script lang="ts">
+export default {
+    name: "Request",
+    components: {
+    }
+}
+</script>
--- frontend/src/views/RequestView.vue
+++ frontend/src/views/RequestView.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
 import DashboardLayout from '@/components/DashboardLayout.vue';
 import BackendService from "../../BackendService"
-import type { Appointment } from "@/classlib/Types"
+import type { Appointment, Job } from "@/classlib/Types"
+import Request from '@/components/Request.vue';
 import { onMounted, ref } from 'vue';
 
 let appointments = ref<Appointment[]>([])
+let jobs = ref<Job[]>([])
 
 onMounted(async () => {
-    const res = await BackendService.getAppointments();
-    console.log(res)
-    appointments.value = [...res?.data]
+    const apptRes = await BackendService.getAppointments();
+    const jobRes = await BackendService.getUpcomingJobs();
+
+    
+    appointments.value = [...apptRes?.data]
+    jobs.value = [...jobRes?.data]
 })
 
 
@@ -34,48 +39,8 @@ onMounted(async () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <!-- row 1 -->
-                        <tr v-for="appointment in appointments" :key="appointment.id">
-                            <th>
-                                <label>
-                                    <input type="checkbox" class="checkbox" />
-                                </label>
-                            </th>
-                            <td>
-                                <div class="flex items-center space-x-3">
-                                    <div class="avatar">
-                                        <!-- <div class="mask mask-squircle w-12 h-12">
-                                            <img src="/tailwind-css-component-profile-2@56w.png"
-                                                alt="Avatar Tailwind CSS Component" />
-                                           
-                                        </div> -->
-                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none"
-                                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                        </svg>
-                                    </div>
-                                    <div>
-                                        <div class="font-bold">
-                                            {{ appointment.customer?.firstName }}
-                                            {{ appointment.customer?.lastName }}
-                                        </div>
-                                        <div class="text-sm opacity-50">Customer</div>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                Assestment of {{ appointment.vehicle?.year }} {{ appointment.vehicle?.make }}
-                                {{ appointment.vehicle?.model }}
-                                <br>
-                                <span class="badge badge-ghost badge-sm">Appointment</span>
-                            </td>
-                            <td>{{ appointment.suggestedDate }}</td>
-                            <th>
-                                <button class="btn btn-ghost btn-xs">Details</button>
-                            </th>
-                        </tr>
-
+                        <Request v-for="job in jobs" :job="job" :key="job.jobNumber"/>
+                        <Request v-for="appointment in appointments" :appointment="appointment" :key="appointment.id"/>
                     </tbody>
 
                 </table>
@@ -88,6 +53,7 @@ onMounted(async () => {
 export default {
     name: "RequestView",
     components: {
+        Request
     }
 }
 </script>
