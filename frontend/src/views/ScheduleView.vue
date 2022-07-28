c870db6329924f93d1a0a7256125a63a508a9742
khalid sheriff <khalalimsheriff@gmail.com>
2022-07-27 19:33:12 -0500

    change to actually bind value to dateTitle

--- frontend/src/views/ScheduleView.vue
+++ frontend/src/views/ScheduleView.vue
@@ -12,41 +12,47 @@
     
     const schedule: MonthBlock = await BackendService.getMonthSchedule(new Date(Date.now()).getMonth(), currentUser.User.id);
     
+    // this works, but it breaks the page because i think it's supposed to be used in <Suspense> tags
     schedule.workingDays.forEach(workingday=>{
         console.log(workingday.day);
         workingday.hourBlocks.forEach(hour=>{
-            console.log(hour.time);
+            console.log("time: " + hour.time);
         })
     });
 
-    
-    
 </script>
 
 <template>
     <NavBar />
+    
+    <Suspense>
+        <template >
+            <div>
+                <button v-if="schedule.month = 0">January</button>
+                <button v-if="schedule.month <= 1">February</button>
+                <button v-if="schedule.month <= 2">March</button>
+                <button v-if="schedule.month <= 3">April</button>
+                <button v-if="schedule.month <= 4">May</button>
+                <button v-if="schedule.month <= 5">June</button>
+                <button v-if="schedule.month <= 6">July</button>
+                <button v-if="schedule.month <= 7">August</button>
+                <button v-if="schedule.month <= 8">September</button>
+                <button v-if="schedule.month <= 9">October</button>
+                <button v-if="schedule.month <= 10">November</button>
+                <button v-if="schedule.month <= 11">December</button>
+            </div>
 
-    <div>
-        <button v-if="schedule.month = 0">January</button>
-        <button v-if="schedule.month <= 1">February</button>
-        <button v-if="schedule.month <= 2">March</button>
-        <button v-if="schedule.month <= 3">April</button>
-        <button v-if="schedule.month <= 4">May</button>
-        <button v-if="schedule.month <= 5">June</button>
-        <button v-if="schedule.month <= 6">July</button>
-        <button v-if="schedule.month <= 7">August</button>
-        <button v-if="schedule.month <= 8">September</button>
-        <button v-if="schedule.month <= 9">October</button>
-        <button v-if="schedule.month <= 10">November</button>
-        <button v-if="schedule.month <= 11">December</button>
-    </div>
+            <div v-for="day in schedule.workingDays">
+                <DayPlanComponent :dayBlock="day"
+                    :dateTitle="new Date( new Date(Date()).getFullYear(), schedule.month, day.day).toDateString()"
+                    />
+            </div>
+        </template>
 
-    <div v-for="day in schedule.workingDays">
-        <DayPlanComponent :dayBlock="day"
-            dateTitle="new Date( new Date(Date()).GetFullYear(), test.month, day.day).toDateString()"
-            />
-    </div>
-
+        <template #fallback>
+            I DON'T EVEN KNOW TBH
+        </template>
+    </Suspense>
 
     <div v-show="" class="modal">
         <!--Placeholder-->
