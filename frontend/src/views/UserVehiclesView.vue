e4dad64211f380d1e4880d788cceb48cdb88338e
Elliot Morrison <elliotmorrison58@gmail.com>
2022-07-28 15:41:22 -0500

    renders vehicles dynamically

--- frontend/src/views/UserVehiclesView.vue
+++ frontend/src/views/UserVehiclesView.vue
@@ -8,18 +8,18 @@
     
 </span>
     <div class="rounded bg-gray-200 w-1/1 my-5 p-5 flex flex-col justify-start gap-y-5 overflow-y-scroll" style="height: 40rem">
-        <div class="w-1/1 flex-none h-fit bg-red-400 rounded min-w-max flex flex-col sm:flex-row p-3 justify-between" v-for="i in 4">
+        <div class="w-1/1 flex-none h-fit bg-red-400 rounded min-w-max flex flex-col sm:flex-row p-3 justify-between" v-for="vehicle in userVehicles">
         <div class="w-48 h-44 bg-white self-center">
             <img src="" alt="">
         </div>
         <div class="sm:grow m-5 text-center sm:text-left">
-            <p class="text-xl sm:text-2xl font-semibold">2012 Honda Civic</p>
-            <p class="sm:text-xl">ID# 1234567</p>
-            <p class="sm:text-xl">Liscense plate #: 2222 HZ</p>
+            <p class="text-xl sm:text-2xl font-semibold">{{`${vehicle.year} ${vehicle.make} ${vehicle.model}`}}</p>
+            <p class="sm:text-xl">ID# {{vehicle.id}}</p>
+            <p class="sm:text-xl">Liscense plate #: {{vehicle.licensePlate}}</p>
         </div>
         <div class="flex flex-row sm:flex-col gap-x-3 justify-evenly">
             <button class="btn btn-sm">view/edit</button>
-            <button class="btn btn-sm">repair history</button>
+            <button class="btn btn-sm">work history</button>
             <button class="btn btn-sm">delete</button>
         </div>
         </div>
@@ -31,11 +31,23 @@
 </template>
 <script lang="ts">
 import DashboardLayout from "../components/DashboardLayout.vue";
+import BackendService from '../../BackendService'
 import { defineComponent } from "vue";
+import type { Vehicle } from "@/classlib/Types";
 export default defineComponent({
     name:'UserVehiclesVeiw.vue',
     components:{
         DashboardLayout,
+    },
+    data(){
+        return{
+            userVehicles : <Vehicle[]>({})
+        }
+    },
+    async created(){
+        let res = await BackendService.getUserVehicles(3)
+        console.log(res?.data);
+        this.userVehicles = res?.data;
     }
 })
 </script>
