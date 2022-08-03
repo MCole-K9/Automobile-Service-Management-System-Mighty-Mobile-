fb9c1263ccf8fb38e13b704c405ecd880b73dd63
Elliot Morrison <elliotmorrison58@gmail.com>
2022-08-03 12:09:27 -0500

    Complete redesign

--- frontend/src/views/UserVehiclesView.vue
+++ frontend/src/views/UserVehiclesView.vue
@@ -7,30 +7,43 @@
     <label for="addVehicleModal" class="btn btn-sm lg:btn-md modal-button">add a Vehicle</label>
 
 </span>
-    <div class="rounded bg-gray-200 w-1/1 my-5 p-5 flex flex-col justify-start gap-y-5 overflow-y-scroll" style="height: 40rem">
-        <div class="w-1/1 flex-none h-fit bg-ourBlue rounded min-w-max flex flex-col sm:flex-row p-3 justify-between" v-for="vehicle in userVehicles">
-        <div class="w-48 h-44 bg-white self-center">
+    <div class="grid grid-cols-1 xl:grid-cols-2 grid-flow-row gap-5 my-10">
+        <div class="bg-ourYellow rounded flex flex-col justify-start items-center md:grid lg:grid xl:flex 2xl:grid grid-rows-3 grid-cols-4 gap-4 h-fit p-4 min-w-96" v-for="vehicle in userVehicles">
+        <div class="row-span-3 bg-white aspect-square h-36 col-span-1">
             <img src="" alt="">
         </div>
-        <div class="sm:grow m-5 text-center sm:text-left">
-            <p class="text-xl sm:text-2xl font-semibold">{{`${vehicle.year} ${vehicle.make} ${vehicle.model}`}}</p>
-            <p class="sm:text-xl">ID# {{vehicle.id}}</p>
-            <p class="sm:text-xl">Liscense plate #: {{vehicle.licensePlate}}</p>
+        <div class="row-span-2 col-span-3 text-center sm:text-left px-3">
+            <p class="font-semibold">{{`${vehicle.year} ${vehicle.make} ${vehicle.model}`}}</p>
+            <!-- <p class="">ID# {{vehicle.id}}</p> -->
+            <p class="">License plate #: {{vehicle.licensePlate}}</p>
         </div>
-        <div class="flex flex-row sm:flex-col gap-x-3 justify-evenly">
-            <button class="btn btn-sm">view/edit</button>
-            <label for="my-modal-3" class="btn btn-sm modal-button" @click="getJobsDoneOnVehicle(vehicle.id)">work history</label>
-            <button class="btn btn-sm btn-error">delete</button>
+        <div class="row-span-2 col-span-3 flex justify-start px-3 gap-x-1">
+            <button class="btn btn-sm flex gap-x-1">
+               <span class="hidden sm-600:block">view</span>
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
+            </button>
+            <button class="btn btn-sm flex gap-x-1">
+               <span class="hidden sm-600:block">edit</span>
+               <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+            </button>
+            <label for="vehicleWorkHistory" class="btn btn-sm modal-button flex gap-x-1" @click="getJobsDoneOnVehicle(vehicle.id)">
+            <span class="hidden sm-600:block">work history</span>
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path></svg>
+            </label>
+            <button class="btn btn-sm btn-error flex gap-x-1">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>
+            </button>
         </div>
         </div>
     </div>
     
 
     <!-- Put this part before </body> tag -->
-    <input type="checkbox" id="my-modal-3" class="modal-toggle" />
-    <div class="modal">
-    <div class="modal-box relative">
-        <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+    <input type="checkbox" id="vehicleWorkHistory" class="modal-toggle" />
+    <div class="modal modal-bottom sm:modal-middle">
+    <div class="modal-box relative sm:w-11/12 sm:max-w-5xl">
+        <label for="vehicleWorkHistory" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+        <h3 class="font-bold text-xl text-center">Work History</h3>
         <div class="overflow-x-auto w-full pt-8">
                 <table class="table w-full">
                     <!-- head -->
@@ -50,24 +63,21 @@
                     <tbody>
                         <Request v-for="job in jobsDoneOnVehicle" :job="job" :key="job.jobNumber"/>
                     </tbody>
-
                 </table>
+                <loadingAnimation v-if="loading" />
+                <p class="text-lg font-medium text-center my-5 w-1/1 opacity-75" v-if="jobsDoneOnVehicle.length == 0 &&!loading">No Jobs Yet</p>
             </div>
     </div>
     </div>
-
     <input type="checkbox" id="addVehicleModal" class="modal-toggle" />
-    <label for="addVehicleModal" class="modal cursor-pointer">
-         <label class="modal-box rounded relative">
-            <AddVehicleComponent/>
-            <div class="modal-action">
-                <button class="btn">Add Vehicle</button>
-            </div>
-        </label>
-    </label>
-    <div class="modal">
-    <div class="modal-box">
-        
+    <div class="modal  modal-bottom sm:modal-middle">
+    <div class="modal-box relative rounded sm:rounded-box sm:w-11/12 sm:max-w-5xl">
+        <label for="addVehicleModal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+        <h3 class="font-bold text-xl text-center my-5">Add new vehicle</h3>
+         <AddVehicleComponent/>
+        <div class="modal-action ">
+         <button class="btn">Add Vehicle</button>
+        </div>
     </div>
     </div>
 </div>
@@ -77,6 +87,7 @@
 </template>
 <script lang="ts">
 import DashboardLayout from "../components/DashboardLayout.vue";
+import loadingAnimation from "@/components/loadingAnimation.vue";
 import BackendService from '../../BackendService'
 import Request from "@/components/Request.vue";
 import AddVehicleComponent from "@/components/AddVehicleComponent.vue";
@@ -84,17 +95,19 @@ import { defineComponent } from "vue";
 import type { Vehicle } from "@/classlib/Types";
 import type { Job } from "@/classlib/Types";
 export default defineComponent({
-    name:'UserVehiclesVeiw.vue',
+    name:'UserVehiclesView.vue',
     components:{
         DashboardLayout,
         Request,
         AddVehicleComponent,
+        loadingAnimation,
     },
     data(){
         return{
             userVehicles : <Vehicle[]>({}),
             allJobs: <Job[]>([]),
             jobsDoneOnVehicle : <Job[]>([]),
+            loading : false,
         }
     },
     async created(){
@@ -104,6 +117,7 @@ export default defineComponent({
     },
     methods:{
         async getJobsDoneOnVehicle(vehicleId:number|undefined){
+            this.loading = true
             this.jobsDoneOnVehicle = []
             let res = await BackendService.getUpcomingJobs()
             this.allJobs = res?.data
@@ -113,8 +127,7 @@ export default defineComponent({
                     console.log(e);
                 }
             })
-            console.log(this.jobsDoneOnVehicle);
-
+            this.loading = false
             
         }
     }
