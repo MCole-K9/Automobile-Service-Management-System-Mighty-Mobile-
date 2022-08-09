473e000d75971754073fd39e8fa6a7cf4fe0b059
Elliot Morrison <elliotmorrison58@gmail.com>
2022-08-09 12:13:20 -0500

    minor design change

--- frontend/src/components/JobComponent.vue
+++ frontend/src/components/JobComponent.vue
@@ -7,18 +7,18 @@
 </script>
 
 <template>
-  <div class="card flex-none bg-gray-200 shadow-xl min-w-64 relative">
-  <div class="card-body">
+  <div class="card flex-none bg-ourYellow shadow-xl w-full relative rounded">
+  <div class="card-body p-5">
     <h2 class="card-title">Job #: {{props.job.jobNumber}}</h2>
-    <p><span class="text-md font-semibold">Service Type: </span> {{props.job.serviceType}}</p>
-    <p><span class="text-md font-semibold">Customer: </span>  {{props.job.vehicle?.owner?.firstName}} {{props.job.vehicle?.owner?.lastName}}</p>
-    <p><span class="text-md font-semibold">Vehicle: </span>{{props.job.vehicle?.year}} {{props.job.vehicle?.make}} {{props.job.vehicle?.model}} </p>
+    <p class="flex justify-between"><span class="text-md font-semibold">Service Type: </span> {{props.job.serviceType}}</p>
+    <p class="flex justify-between"><span class="text-md font-semibold">Customer: </span>  {{props.job.vehicle?.owner?.firstName}} {{props.job.vehicle?.owner?.lastName}}</p>
+    <p class="flex justify-between"><span class="text-md font-semibold">Vehicle: </span>{{props.job.vehicle?.year}} {{props.job.vehicle?.make}} {{props.job.vehicle?.model}} </p>
     <div class="card-actions justify-center mt-3">
-      <button class="btn btn-sm bg-ourYellow" @click="$router.push(`/dashboard/viewjob/${props.job.jobNumber}`)">View</button>
+      <button class="btn btn-sm" @click="$router.push(`/dashboard/viewjob/${props.job.jobNumber}`)">View</button>
     </div>
   </div>
-  <span class="badge absolute top-8 right-4 px-4 bg-ourRed" v-if="!props.job.completed">{{props.job.confirmed? "Confirmed": "Pending"}}</span>
-  <span class="badge absolute top-8 right-4 text-black px-4 bg-success" v-else-if="props.job.completed">completed</span>
+  <span class="badge absolute top-4 right-4 px-4 bg-ourRed" v-if="!props.job.completed">{{props.job.confirmed? "Confirmed": "Pending"}}</span>
+  <span class="badge absolute top-4 right-4 text-black px-4 bg-success" v-else-if="props.job.completed">completed</span>
 </div>
 </template>
 
