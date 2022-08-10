4d6d532bbca0751c092243e380b380550224a8f4
Elliot Morrison <elliotmorrison58@gmail.com>
2022-08-09 19:04:15 -0500

    clear dashboard til we find something to put on it

--- frontend/src/components/UserDashboard.vue
+++ frontend/src/components/UserDashboard.vue
@@ -4,41 +4,7 @@
                 <p class="text-gray-800 text-xl sm:text-2xl lg:text-4xl font-bold">Dashboard</p>
                 <button class="btn btn-sm lg:btn-md" @click="$router.push('dashboard/vehicles')">view vehicles</button>
             </span>
-        <div class="flex lg:flex-row flex-col w-full  h-fit gap-5 lg:justify-between">
-            <div class="rounded grow h-96 relative ">
-                <div class="rounded bg-gray-800 w-1/1 h-16 flex justify-center items-center mb-3">
-                    <p class="text-xl lg:text-2xl font bold text-white">Requests For Appointment</p>
-                </div>
-                <div class="overflow-x-auto w-full">
-                <table class="table w-full overflow-y-scroll">
-                    <!-- head -->
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th class="text-center">Created By</th>
-                            <th class="text-center">For</th>
-                            <th class="text-center">Date</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <Request v-for="app,index in UserAppointments" :appointment="app" :index="index"/>
-                    </tbody>
-
-                </table>
-            </div>
-            </div>
-
-        <div class="bg-gray-200 rounded grow h-96 relative overflow-y-scroll">
-            <div class="rounded bg-gray-800 w-1/1 h-16 flex sticky top-0  justify-center items-center mb-3">
-                <p class="text-xl lg:text-2xl font bold text-white">Ongoing Repairs/Maintenance</p>
-            </div>
-            <div class="bg-green-500 min-w-max h-20 rounded mb-3 w-1/1 mx-3" v-for="i in 5">
-
-                </div>
-        </div>
-
-        </div>
+            
     </div>
 </template>
 <script lang="ts">
