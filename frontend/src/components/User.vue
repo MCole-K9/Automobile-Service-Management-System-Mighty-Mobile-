f48adcd373c3e49b230d19f79fdf130a06232e27
Elliot Morrison <elliotmorrison58@gmail.com>
2022-08-11 12:49:26 -0500

    added optional prop

--- frontend/src/components/User.vue
+++ frontend/src/components/User.vue
@@ -2,7 +2,7 @@
 
  const props = defineProps<{
     User: User,
-    
+    forDisplay?: boolean
  }>()
 
 
@@ -12,19 +12,19 @@
     <div class="mx-auto max-w-3xl shadow-lg rounded-lg p-10 grid grid-cols-2 gap-4">
         <div class="mx-auto flex flex-col space-y-2 w-full">
             <label class="text-center w-full  py-2 px-2 bg-ourGrey shadow-lg" for="vmake">First Name</label>
-            <input class="input w-full input-bordered " type="text" v-model="props.User.firstName" id="vmake">
+            <input class="input w-full input-bordered my-3" type="text" v-model="props.User.firstName" id="vmake" :disabled="props.forDisplay">
         </div>
         <div class="mx-auto flex flex-col space-y-2 w-full">
             <label class="text-center w-full  py-2 px-2 bg-ourGrey shadow-lg" for="vmake">Last Name</label>
-            <input class="input w-full input-bordered " type="text" v-model="props.User.lastName"  id="vmake">
+            <input class="input w-full input-bordered my-3" type="text" v-model="props.User.lastName"  id="vmake" :disabled="props.forDisplay">
         </div>
         <div class="mx-auto flex flex-col space-y-2 w-full col-span-2">
             <label class="text-center w-full  py-2 px-2 bg-ourGrey shadow-lg" for="vmake">Email</label>
-            <input class="input w-full input-bordered " type="text" v-model="props.User.email" id="vmake">
+            <input class="input w-full input-bordered my-3" type="text" v-model="props.User.email" id="vmake" :disabled="props.forDisplay">
         </div>
         <div class="mx-auto flex flex-col space-y-2 w-full col-span-2">
             <label class="text-center w-full  py-2 px-2 bg-ourGrey shadow-lg" for="vmake">Phone Number</label>
-            <input class="input w-full input-bordered " type="text" v-model="props.User.phoneNumber"  id="vmake">
+            <input class="input w-full input-bordered my-3" type="text" v-model="props.User.phoneNumber"  id="vmake" :disabled="props.forDisplay">
         </div>
         <slot name="Extra"></slot>
     </div>
