e3ecbe9e37c10c4cd056901fddcae6950cb08722
Elliot Morrison <elliotmorrison58@gmail.com>
2022-08-15 15:28:38 -0500

    test change

--- frontend/src/components/MechanicDashboard.vue
+++ frontend/src/components/MechanicDashboard.vue
@@ -4,7 +4,18 @@
                 <p class="text-gray-800 text-xl sm:text-2xl lg:text-4xl font-bold">Dashboard</p>
                 <button class="btn btn-sm lg:btn-md" @click="$router.push('dashboard/jobboard')">view job board</button>
             </span>
-            
+            <span>
+                <FormKit
+                type="datetime-local"
+                value="2020-03-13T18:22"
+                label="End of the world"
+                help="When will the end of the world take place?"
+                validation="required|after"
+                validation-visibility="live"
+                v-model="test"
+                />
+                <button class="btn btn-sm m-5" @click="$log(new Date('2022-08-15T15:18'))">test</button>
+            </span>
     </div>
 </template>
 <script lang="ts">
@@ -13,6 +24,7 @@ import { defineComponent } from "vue"
 import Request from "./Request.vue"
 import type { Appointment } from '../classlib/Types';
 import { currentUserStore } from "@/stores/User";
+// import { Form }
 
 const currentUser = currentUserStore();
 
@@ -23,7 +35,8 @@ export default defineComponent({
     },
     data(){
         return{
-            UserAppointments : <Appointment[]>([])
+            UserAppointments : <Appointment[]>([]),
+            test : <any>({})
         }
     },
     async created(){
@@ -33,4 +46,9 @@ export default defineComponent({
         this.UserAppointments = this.UserAppointments.filter(e => e.customerId == currentUser.User.id)
     }
 })
+declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $log(anything:any) : void
+  }
+}
 </script>
