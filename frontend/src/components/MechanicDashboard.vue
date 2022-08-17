21ce7d080789327fa1fe6a58b8ef29083956b9e7
Elliot Morrison <elliotmorrison58@gmail.com>
2022-08-16 20:14:46 -0500

    yellow

--- frontend/src/components/MechanicDashboard.vue
+++ frontend/src/components/MechanicDashboard.vue
@@ -2,7 +2,7 @@
     <div class="px-5">
             <span>
                 <div class="h-fit min-h- w-1/1 bg-ourYellow shadow rounded-lg p-2 gap-y-3 flex flex-col justify-start">
-                    <span class="flex flex-col font-bold text-2xl p-3 bg-gray-800 rounded text-gray-200">
+                    <span class="flex flex-col font-bold text-2xl p-3 bg-yellow-600 rounded">
                         <span class="flex justify-between">
                             <label class="">Current Jobs</label>
                             <button class="btn btn-sm" @click="currentJobsToggle = !currentJobsToggle">
@@ -11,9 +11,9 @@
                                 <svg v-if="currentJobsToggle" class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7l4-4m0 0l4 4m-4-4v18"></path></svg>
                             </button>
                         </span>
-                        <div class="w-1/1 bg-gray-200 h-64 mt-3" v-if="currentJobsToggle"> </div>
+                        <div class="w-1/1 bg-white h-64 mt-3" v-if="currentJobsToggle"> </div>
                     </span>
-                    <span class="flex justify-between font-bold text-2xl p-3 bg-gray-800 rounded text-gray-200">
+                    <span class="flex justify-between font-bold text-2xl p-3 bg-yellow-600  rounded">
                         <label for="">Job History</label>
                         <button class="btn btn-sm">view</button>
                     </span>
